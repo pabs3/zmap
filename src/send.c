50789dd7deb30d7381b2d0a1b6a7f90f21481fb2
Paul Wise <pabs3@bonedaddy.net>
2015-09-18 12:11:29 +0200

    Fix some typos in comments and documentation
    Suggested-by: codespell --quiet-level=3
    ./CHANGELOG:28: shoudl  ==> should
    ./CHANGELOG:117: somtimes  ==> sometimes
    ./lib/constraint.c:332: initally  ==> initially
    ./lib/logger.c:69: accross  ==> across
    ./lib/util.h:33: priviledges  ==> privileges
    ./test/test_sharding.py:18: convertion  ==> convention
    ./examples/forge-socket/README:71: occurances  ==> occurrences
    ./src/zmap.1.ronn:186: sucessful  ==> successful
    ./src/zmap.1.html:203: sucessful  ==> successful
    ./src/zmap.1:195: sucessful  ==> successful
    ./src/ztee.c:54: indicies  ==> indices
    ./src/send.c:225: inital  ==> initial

--- src/send.c
+++ src/send.c
@@ -226,7 +226,7 @@ int send_run(sock_t st, shard_t *s)
 		delay = 10000;
 
         if (send_rate < slow_rate) {
-            // set the inital time difference
+            // set the initial time difference
             sleep_time = nsec_per_sec / send_rate;
             last_time = now() - (1.0 / send_rate);
         } else {
